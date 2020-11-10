40d3a4a78ef8885589fd8e3d75899621a1c44b77
github username <63208817+Thea-J@users.noreply.github.com>
2020-11-10 23:49:57 +0100

    Function & if ... else

--- helloWorld.js
+++ helloWorld.js
@@ -1,1 +1,12 @@
 " Hello World! "
+
+// Allows us to load the contents of another file in the same directory into the file we are working on 
+// require(“./file_name.js”) 
+
+
+
+function talkToGran(string) {
+if (string.toLowerCase() == string) {return "I can't hear you"}
+else if (string.toUpperCase() == string) {return "Yes indeed"} 
+else if (string == "I love you") {return "I love you too"}
+}   
