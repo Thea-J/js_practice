e3d060a47dc7612ed3e1aa3874aaded3b2854618
github username <63208817+Thea-J@users.noreply.github.com>
2020-11-12 23:36:21 +0100

    Longest Substring Without Repeating Characters
    lengthOfLongestSubstring function expression

---
+++ longestSubstring.js
@@ -0,0 +1,9 @@
+// Longest Substring Without Repeating Characters - Brute force approach
+
+//Given a string s, find the length of the longest substring without repeating characters.
+//string s consists of English letters, digits, symbols and spaces
+
+//Declare the function lengthOfLongestSubstring via the function expression
+var lengthOfLongestSubstring = function(s) {
+    
+};
