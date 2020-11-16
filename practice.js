995c5f01c3385e1474e81ca5325109a0b408373c
github username <63208817+Thea-J@users.noreply.github.com>
2020-11-16 13:24:29 +0100

    Self-computing my code with an example to see where error is

--- practice.js
+++ practice.js
@@ -1,5 +1,11 @@
 //Practice code goes here
 
-//example: let string = "dvdf" < lengthOfLongestSubstring = 3 for substring "vdf"
+//example: let string = "a b c a b c b b" < lengthOfLongestSubstring = 3 for substring "vdf"
+//example: let string = "0 1 2 3 4 5 6 7"
+ // 1: sub = "abc" stop at i=3 back to i=1
+ // 2: sub = "bca" stop at i=4 back to i=2
+ // 3: sub = "cab" stop at i=5 back to i=3
+ // 4: sub = "abc" stop at i=6 back to i=1 << should be going back to i=4
 
+ 
 
