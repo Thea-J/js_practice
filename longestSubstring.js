7c9f4ea66a016adcfa6b888b5e1acb08dd3e92c5
github username <63208817+Thea-J@users.noreply.github.com>
2020-11-21 18:32:25 +0100

    Complete: returns the length of the longest unique substing

--- longestSubstring.js
+++ longestSubstring.js
@@ -11,7 +11,7 @@ function longest_substring_from_array(array_of_substrings) {
             longest_substring_length = array_of_substrings[j].length
         }
     }
-    console.log(`The largest substring has length property =  ${longest_substring_length} `)
+    // console.log(`The largest substring has length property =  ${longest_substring_length} `)
     return longest_substring_length
 }
 
@@ -20,18 +20,20 @@ var lengthOfLongestSubstring = function(s) {
     let array_of_substrings = [];
     let substring = ""
 
-    //Split any given string into an array of all possible substrings - including the case of the empty string
+    //Split any given string into an array of all possible unique substrings - including the case of the empty string
     if (typeof(s) == "string" && s.length == 0) { 
         array_of_substrings.push(s)
         return s.length
     } else {
         for (let i=0; i<s.length; i++) {
+            // If the substring includes the current character, stop execution, add the substring to the array_of_substrings
+            // Find where the most recent repeated character is in the string & restart execution at the character DIRECTLY AFTER that
             if (substring.includes(s[i])) {
                 array_of_substrings.push(substring)
                 let repeated_character_index = s.slice(0,i).lastIndexOf(s[i])
-                console.log(`FOUND A REPEATED CHARACTER ${s[i]} AT Index ${i} THIS WAS PREVIOUSLY AT INDEX ${repeated_character_index}`)
+                // console.log(`FOUND A REPEATED CHARACTER ${s[i]} AT Index ${i} THIS WAS PREVIOUSLY AT INDEX ${repeated_character_index}`)
                 i = repeated_character_index + 1
-                console.log(`Going back to index ${i}`)
+                // console.log(`Going back to index ${i}`)
                 substring = substring.replace(substring, s[i])
                 if (i == s.length-1) {array_of_substrings.push(s[i])}
             } else { 
@@ -39,7 +41,15 @@ var lengthOfLongestSubstring = function(s) {
                 if (i == s.length-1) {array_of_substrings.push(substring)}
             }
         }
-     console.log(`Array is currently ${array_of_substrings}`)
+    //  console.log(`Array is currently ${array_of_substrings}`)
     }
-     longest_substring_from_array(array_of_substrings)
+    return longest_substring_from_array(array_of_substrings)
 };
+
+// Test cases:
+// let s = "abcabcbb"
+// let s = "bbbbb" 
+// let s = "pwwkew" 
+// let s = "" 
+// let s = "dvdf" 
+
--- practice.js
+++ practice.js
@@ -1,28 +1,39 @@
-//Practice goes here
+// Longest Substring Without Repeating Characters - Brute force approach
+
+//Given a string s, find the length of the longest substring without repeating characters.
+//string s consists of English letters, digits, symbols and spaces
+
+//Find the substring in the array that has the longest length property 
+function longest_substring_from_array(array_of_substrings) {
+    longest_substring_length = 0; 
+    for (j=0; j<array_of_substrings.length; j++) {
+        if (array_of_substrings[j].length > longest_substring_length) {
+            longest_substring_length = array_of_substrings[j].length
+        }
+    }
+    return longest_substring_length
+}
 
 var lengthOfLongestSubstring = function(s) {
     let array_of_substrings = [];
     let substring = ""
 
-    for (let i=0; i<s.length; i++){
-        if (substring.includes(s[i])){
-            array_of_substrings.push(substring)
-            console.log(`FOUND A REPEATED CHARACTER ${s[i]} AT Index ${i} & Array is currently ${array_of_substrings}`)
-
-            let repeated_character_index = s.slice(0,i).lastIndexOf(s[i])
-            console.log(`THIS WAS PREVIOUSLY AT INDEX ${repeated_character_index}`)
-            i = repeated_character_index + 1
-            console.log(`Going back to index ${i}`)
-            substring = substring.replace(substring, s[i])
-
-
-
-
-            if (i == s.length-1) {array_of_substrings.push(s[i])}
-        } else { 
-            substring = substring + s[i]   
-            if (i == s.length-1) {array_of_substrings.push(substring)}
+    if (typeof(s) == "string" && s.length == 0) { 
+        array_of_substrings.push(s)
+        return s.length
+    } else {
+        for (let i=0; i<s.length; i++) {
+            if (substring.includes(s[i])) {
+                array_of_substrings.push(substring)
+                let repeated_character_index = s.slice(0,i).lastIndexOf(s[i])
+                i = repeated_character_index + 1
+                substring = substring.replace(substring, s[i])
+                if (i == s.length-1) {array_of_substrings.push(s[i])}
+            } else { 
+                substring = substring + s[i]   
+                if (i == s.length-1) {array_of_substrings.push(substring)}
+            }
         }
     }
-    console.log(`Array is currently ${array_of_substrings}`)
-}
+    return longest_substring_from_array(array_of_substrings)
+};
