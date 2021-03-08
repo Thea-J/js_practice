24939e8d4818372aee636a7bad594a05b586e484
github username <63208817+Thea-J@users.noreply.github.com>
2021-03-08 17:54:34 +0100

    Plan for how to return a complement string

---
+++ dnaString.js
@@ -0,0 +1,13 @@
+function DNAStrand(dna){
+    // Given a string of dna, return the complement string
+    // Where "A" & "T" are intechangeable as are "C" and "G" 
+
+    //Iterate over the string dna 
+    //wite conditionals such that substrings "A" & "T" are swapped and substrings "C" and "G" 
+    
+}
+
+//Test Cases:
+//Test.assertEquals(DNAStrand("AAAA"),"TTTT","String AAAA is");
+//Test.assertEquals(DNAStrand("ATTGC"),"TAACG","String ATTGC is");
+//Test.assertEquals(DNAStrand("GTAT"),"CATA","String GTAT is");
