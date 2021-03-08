f5ca51c1445736c565a1a486c9eb1aa221b84970
github username <63208817+Thea-J@users.noreply.github.com>
2021-03-08 21:32:36 +0100

    Done

--- dnaString.js
+++ dnaString.js
@@ -1,13 +1,37 @@
 function DNAStrand(dna){
-    // Given a string of dna, return the complement string
-    // Where "A" & "T" are intechangeable as are "C" and "G" 
+    // Given a string of dna, return the complement string, where "A" & "T" are interchangeable as are "C" and "G" 
 
-    //Iterate over the string dna 
-    //wite conditionals such that substrings "A" & "T" are swapped and substrings "C" and "G" 
-    
+    //Define an empty string
+    let complementString = "";
+
+    // Iterate over the string dna 
+    // Populate the string complementString with characters from the string dna
+    // Wite conditionals such that substrings "A" & "T" are swapped and substrings "C" and "G" 
+
+    for (let i=0; i<dna.length; i+=1){
+
+        if (dna[i] === "A"){
+            complementString = complementString.concat("T")
+        }
+        else if (dna[i] === "T"){
+            complementString = complementString.concat("A")
+        }
+        else if (dna[i] === "C"){
+            complementString = complementString.concat("G")
+        }
+        else if (dna[i] === "G"){
+            complementString = complementString.concat("C")
+        }
+        else { complementString = complementString.concat(dna[i])
+        }
+    }
+    return complementString
 }
 
 //Test Cases:
 //Test.assertEquals(DNAStrand("AAAA"),"TTTT","String AAAA is");
 //Test.assertEquals(DNAStrand("ATTGC"),"TAACG","String ATTGC is");
 //Test.assertEquals(DNAStrand("GTAT"),"CATA","String GTAT is");
+
+//Print in the terminal the return value of the function solution
+console.log(DNAStrand("AAAA"))
