36d6e9929756263710600a036a636cfbca84ae0c
github username <63208817+Thea-J@users.noreply.github.com>
2021-03-09 16:05:31 +0100

    plan

---
+++ seriesSum.js
@@ -0,0 +1,15 @@
+// Return the sum of the series (1 + 1/4 + 1/7 + 1/10 + 1/13 + ...) up to nth term 
+// If the given value is 0 then it should return 0.00
+// Round answers to 2 decimal places & return it as String
+
+function SeriesSum(n){
+  //If n === 0, return 0.00
+  //Otherwise, define the series formula
+  //Return answers in 2 dp as strings 
+}
+
+//Test Cases:
+// Test.assertEquals(SeriesSum(1), "1.00")
+// Test.assertEquals(SeriesSum(2), "1.25")
+// Test.assertEquals(SeriesSum(3), "1.39")
+// Test.assertEquals(SeriesSum(4), "1.49")
