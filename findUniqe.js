23ab8d0ceab4b1e3981c9687c877ff5afdf98419
github username <63208817+Thea-J@users.noreply.github.com>
2021-03-11 17:06:45 +0100

    plan

---
+++ findUniqe.js
@@ -0,0 +1,17 @@
+//Find the unique element in an array
+
+function findUniq(arr) {
+    // Define a comparison variable with a null value
+    // Iterate over the given array
+    // Set the comparison variable to the first array element
+    // Compare the array elements to the comparison variable 
+    // Reassign the value of the comparison variable if it differs from the other array elements
+}
+
+//Test Cases:
+// Test.assertEquals(findUniq([ 0, 1, 0 ]), 1);
+// Test.assertEquals(findUniq([ 1, 1, 1, 2, 1, 1 ]), 2);
+// Test.assertEquals(findUniq([ 3, 10, 3, 3, 3 ]), 10);
+
+//Print in the terminal the return value of the function solution
+console.log(findUniq([ 0, 1, 0 ]))
